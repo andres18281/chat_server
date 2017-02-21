4c12669c140508551b142c2420a54ebb1d8ade82
Claudio andres giraldo <andres18281@hotmail.com>
2017-02-21 14:35:25 -0500

    Update chat2.js

--- chat/chat2.js
+++ chat/chat2.js
@@ -2,22 +2,24 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var socket = require('socket.io')(http);
+
 var people = {};
 socket.on("connection", function (client){
     client.on("join", function(name){
           if(name.id != null){ // si existe un id enviado por el cliente, no genera id
             var unique_id = name.id;
             client.id = unique_id.replace("/#","");
-            people[client.id] = {"id":client.id,"nombre":name.name};
+            people[client.id] = {"id":client.id,"nombre":name.name,"cargo":name.tipo};
           }else{ // en caso de que no exisa el id, socket.io genera el id
             var unique_id = client.id;
             client.id = unique_id.replace("/#","");
-            people[client.id] = {"id":client.id,"nombre":name.name};
+            people[client.id] = {"id":client.id,"nombre":name.name,"cargo":name.tipo};
           }
-          client.emit("id_user",{"id":people[client.id].id,"nombre":name.name});
+          client.emit("id_user",{"id":people[client.id].id,"nombre":name.name,"cargo":name.tipo});
           socket.sockets.emit("update-people", people);
           console.log(people);
     });
+
     client.on("chat", function(msg){
       console.log(msg);
       for(data in people){
@@ -31,15 +33,17 @@ socket.on("connection", function (client){
     });
 
     client.on("disconnect", function(){
-      setTimeout(function () {
-        if (!people[client.id]) {
+      //setTimeout(function () {
+        // body...
+        //if (!people[client.id]) {
+          //socket.sockets.emit("update", people[client.id].name + " has left the server.");
+          console.log("ha dejado "+people[client.id].name);
           delete people[client.id];
           socket.sockets.emit("update-people", people);
-        };
-      }, 3000);
+        //};
+      //}, 3000);
     });
 });
 http.listen(1223, function(){
  console.log('listening on *:1223');
 });
-
